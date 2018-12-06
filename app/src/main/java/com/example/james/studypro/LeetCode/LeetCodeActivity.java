b72ae3fc5c6c7518e36ff6a099879f206bc3ee61
夏星宇 <james@xiaxingyudeMacBook-Air.local>
2018-12-06 16:02:10 +0800

    update

--- app/src/main/java/com/example/james/studypro/LeetCode/LeetCodeActivity.java
+++ app/src/main/java/com/example/james/studypro/LeetCode/LeetCodeActivity.java
@@ -6,8 +6,11 @@ import android.util.Log;
 
 import com.example.james.studypro.R;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class LeetCodeActivity extends AppCompatActivity {
@@ -314,6 +317,25 @@ public class LeetCodeActivity extends AppCompatActivity {
     }
 
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (queue.size() != 0){
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            result.add(0,list);
+        }
+        return result;
     }
 }
