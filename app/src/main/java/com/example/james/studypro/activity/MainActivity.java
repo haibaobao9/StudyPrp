018d63eeadc5f4531eb985231ad385b8fb69b0cd
xiaxingyu <xy.xia@yuneec.com>
2019-02-12 11:43:31 +0800

    add .gitignore

--- app/src/main/java/com/example/james/studypro/activity/MainActivity.java
+++ app/src/main/java/com/example/james/studypro/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.james.studypro;
+package com.example.james.studypro.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -6,8 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.james.studypro.LeetCode.LeetCodeActivity;
-import com.example.james.studypro.View.ViewActivity;
+import com.example.james.studypro.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn_leet_code,btn_view;
